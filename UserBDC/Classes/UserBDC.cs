dc98dbb6d02337c527fb1d343f29caafad910efa
piyushaggarwal02 <piyush.aggarwal02@nagarro.com>
2020-03-30 23:20:38 +0530

    first commit

---
+++ UserBDC/Classes/UserBDC.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using UserBDC.Interfaces;
+
+namespace UserBDC.Classes
+{
+    public class UserBDC : IUserBDC
+    {
+        public UserDTO userDetails;
+
+        public UserBDC()
+        {
+            CreateDummyUser();
+        }
+
+        public void CreateDummyUser()
+        {
+            UserDTO user1 = new UserDTO
+            {
+                Name = "Piyush Aggarwal",
+                Age = 26,
+                Email = "piyush.aggarwal02@nagarro.com"
+            };
+
+            userDetails = user1;
+        }
+
+        public UserDTO GetUserDetails(int userId)
+        {
+            return userDetails;
+        }
+    }
+}
---
+++ UserBDC/Interfaces/IUserBDC.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserBDC.Interfaces
+{
+    public interface IUserBDC
+    {
+        UserDTO GetUserDetails(int userId);
+    }
+}
